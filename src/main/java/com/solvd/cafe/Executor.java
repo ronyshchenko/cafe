42e4db293ea739d5f1b5a5ef3930bf49a15a4652
ronyshchenko <roma.onyshchenko@gmail.com>
2022-05-12 19:46:43 +0300

    new1 changed

--- src/main/java/com/solvd/cafe/Executor.java
+++ src/main/java/com/solvd/cafe/Executor.java
@@ -3,16 +3,19 @@ package com.solvd.cafe;
 import com.mysql.jdbc.Driver;
 import com.solvd.cafe.Util.ConnectionManager;
 import com.solvd.cafe.dao.jdbcMySQLImpl.CafeDao;
+import com.solvd.cafe.models.CafeModel;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 import java.sql.SQLException;
 
 public class Executor {
-//    private static final Logger LOGGER = LogManager.getLogger(Executor.class);
+    private static final Logger LOGGER = LogManager.getLogger(Executor.class);
 
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
-        String us = "use game;";
+        String us = "use cafeshka;";
         String sql = """
                 CREATE TABLE IF NOT EXISTS info (
                 id SERIAL PRIMARY KEY,
@@ -25,15 +28,16 @@ public class Executor {
                 ('test2');
                 """;
         try (var connection = ConnectionManager.open();
-             var statement = connection.createStatement()) {
+            var statement = connection.createStatement()) {
             System.out.println(connection.getSchema());
-            System.out.println("romaroma" + connection.getTransactionIsolation());
+            System.out.println(connection.getTransactionIsolation());
             var executeResult1 = statement.execute(us);
-            var executeResult = statement.execute(sql);
-            var executeResult2 = statement.execute(sql1);
-            System.out.println(executeResult);
+            //var executeResult = statement.execute(sql);
+            //var executeResult2 = statement.execute(sql1);
+            //System.out.println(executeResult);
             CafeDao cafeDao = new CafeDao();
-            System.out.println(cafeDao.delete(1));
+            cafeDao.create(2, "sadd", "dsaasda", "erwr", "ewrwr");
+            //System.out.println(cafeDao.delete(1));
 
 
         }
--- src/main/java/com/solvd/cafe/dao/jdbcMySQLImpl/CafeDao.java
+++ src/main/java/com/solvd/cafe/dao/jdbcMySQLImpl/CafeDao.java
@@ -12,14 +12,24 @@ import java.sql.*;
 public class CafeDao implements ICafeDao {
 
     private static final String DELETE_SQL = "DELETE FROM cafe WHERE id = ?";
-
+    private static final String SAVE_SQL = "INSERT INTO cafe (id, name, address, site, mail) VALUES (?,?,?,?,?)";
 
     private static final Logger LOGGER = LogManager.getLogger(CafeDao.class);
+
     @Override
     public void create(int id, String address, String email, String name, String site) {
+        try(var connection = ConnectionManager.open();
+            var preparedStatement = connection.prepareStatement(SAVE_SQL)){
+            preparedStatement.setString(1, String.valueOf(id));
+            preparedStatement.setString(2, address);
+            preparedStatement.setString(3, email);
+            preparedStatement.setString(4, name);
+            preparedStatement.setString(5, site);
 
-    }
-
+        } catch (SQLException throwables) {
+        throw new DaoExeption(throwables);
+        }
+}
     @Override
     public void update(String appointmentDate, int id) {
 
